2729bb06e6e5119f8c4c747e3cba863f2d8533cc
sujan.sun <sunshore927@163.com>
2017-07-31 12:55:22 +0800

    add 'createdTime' to dispay in Admin Blog page

--- myproject/myblog/admin.py
+++ myproject/myblog/admin.py
@@ -3,4 +3,9 @@ from django.contrib import admin
 # Register your models here.
 from .models import Category, Tag, Blog
 
-admin.site.register([Category,Tag, Blog])
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('title','createdTime')
+
+admin.site.register(Blog, BlogAdmin)
+admin.site.register(Category)
+admin.site.register(Tag)
